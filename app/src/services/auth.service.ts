08b9c6f80bd3f61280ae8a7582de9f3a90f8e823
Ernest <ealgenv@gmail.com>
2023-08-27 12:40:11 +0300

    .

--- app/src/services/auth.service.ts
+++ app/src/services/auth.service.ts
@@ -3,9 +3,13 @@ import axios from "axios";
 
 export const getAuth = {
 	async addUser(user: IAddUser) {
-		const { data } = await axios.post(`http://localhost:3000/api/user`, user, {
-			headers: { "Content-Type": "application/json" },
-		});
+		const { data } = await axios.post(
+			`${window.location.origin}/api/user`,
+			user,
+			{
+				headers: { "Content-Type": "application/json" },
+			}
+		);
 
 		return data;
 	},
