b933c150fe0bb7610aed46b6e5090a5e0fb96575
Ernest <ealgenv@gmail.com>
2023-08-27 13:58:55 +0300

    ...

--- app/src/app/auth/signin/page.tsx
+++ app/src/app/auth/signin/page.tsx
@@ -1,7 +1,16 @@
 import { Illustration, SignInForm } from "@/widgets";
 import styles from "./page.module.scss";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
 
-export default function SignIn() {
+export default async function SignIn() {
+	const session = await getServerSession(options);
+
+	if (session?.user) {
+		redirect("/admin");
+	}
+
 	return (
 		<main className={styles.main}>
 			<SignInForm />
